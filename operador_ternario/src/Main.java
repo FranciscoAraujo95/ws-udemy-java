23259d5a40edb38e43f251d55b24f3871820f0ac
Francisco <fhydglan@gmail.com>
2024-07-07 14:19:49 -0300

    made the exemple for this exercise

--- operador_ternario/src/Main.java
+++ operador_ternario/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int idade = 18;
+        String beber = (idade >= 18) ? "Pode beber" : "Não pode beber";
+        System.out.println(beber);
     }
 }
