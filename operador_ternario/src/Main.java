6d3f7bbcc01c642a6844645b3f0b38ec141425fc
Francisco <fhydglan@gmail.com>
2024-07-07 14:50:20 -0300

    made another example

--- operador_ternario/src/Main.java
+++ operador_ternario/src/Main.java
@@ -12,5 +12,19 @@ public class Main {
 
         sc.close();
 
+        //Another example
+//        double price = 33.45;
+//        double descount;
+//        if (price < 20) {
+//            descount = price * 0.1;
+//        } else {
+//            descount = price * 0.05;
+//        }
+
+        //We could to the same thing this way
+        double price = 33.45;
+        double discount = (price < 20.0) ? price * 0.1 : price * 0.05;
+        System.out.printf("Final price = %.2f%n", price - discount);
+
     }
 }
