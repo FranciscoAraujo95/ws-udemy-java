ba1bffef8daadb7ac9a1e972df3126ccdd6b5a48
Francisco <fhydglan@gmail.com>
2024-07-10 13:58:52 -0300

    exercise teacher version

--- Section-9-Constructors/exercise_001_teacher_version/src/entities/Account.java
+++ Section-9-Constructors/exercise_001_teacher_version/src/entities/Account.java
@@ -2,47 +2,57 @@ package entities;
 
 public class Account {
 
-  //Attributes
-  private int number;
-  private String holder;
-  private double balance;
+    //Attributes
+    private int number;
+    private String holder;
+    private double balance;
 
-  //Constructors
-  public Account(int number, String holder) {
-    this.number = number;
-    this.holder = holder;
-  }
+    //Constructors
+    public Account(int number, String holder) {
+        this.number = number;
+        this.holder = holder;
+    }
 
-  public Account(int number, String holder, double initialDeposit) {
-    this.number = number;
-    this.holder = holder;
-    deposit(initialDeposit);
-  }
+    public Account(int number, String holder, double initialDeposit) {
+        this.number = number;
+        this.holder = holder;
+        deposit(initialDeposit);
+    }
 
-  //Methods
-  public void deposit(double amount) {
-    balance += amount;
-  }
+    //Methods
+    public void deposit(double amount) {
+        balance += amount;
+    }
 
-  public void withdraw(double amount) {
-    balance -= amount + 5;
-  }
+    public void withdraw(double amount) {
+        balance -= amount + 5;
+    }
 
-  //Getters & Setters
-  public int getNumber() {
-    return number;
-  }
+    //Getters & Setters
+    public int getNumber() {
+        return number;
+    }
 
-  public String getHolder() {
-    return holder;
-  }
+    public String getHolder() {
+        return holder;
+    }
 
-  public void setHolder(String holder) {
-    this.holder = holder;
-  }
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
 
-  public double getBalance() {
-    return balance;
-  }
+    public double getBalance() {
+        return balance;
+    }
 
+    @Override
+    public String toString() {
+        return "Account "
+                + number
+                + ", Holder: "
+                + holder
+                + ", Balance: $ "
+                + String.format("%.2f", balance)
+                + "\n";
+    }
 }
