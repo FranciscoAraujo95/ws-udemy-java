260c529c1e164208848efa9333040a5f543f3aef
Francisco <fhydglan@gmail.com>
2024-07-09 21:30:15 -0300

    generating constructors / getters & setters

--- Section-9-Constructors/encapsulation/src/entities/Product.java
+++ Section-9-Constructors/encapsulation/src/entities/Product.java
@@ -6,6 +6,8 @@ public class Product {
     private double price;
     private int quantity;
 
+    public Product() {
+    }
 
     public Product(String name, double price, int quantity) {
         this.name = name;
