2e73e30468f9ac93b38eaf48ac0ce738db4c530d
Francisco <fhydglan@gmail.com>
2024-07-12 12:08:23 -0300

    solved exercise number two

--- section-10-memorybehavior-arrays-lists/exercises/ex_002/src/application/Program.java
+++ section-10-memorybehavior-arrays-lists/exercises/ex_002/src/application/Program.java
@@ -10,24 +10,41 @@ import java.util.Arrays;
             Locale.setDefault(Locale.US);
             Scanner sc = new Scanner(System.in);
 
-            System.out.print("How many numbers do you want to check? ");
-            int n = sc.nextInt();
-            double[] vector = new double[n];
+            String response = "y";
+            do {
+                System.out.print("How many numbers do you want to check? ");
+                int n = sc.nextInt();
+                double[] vector = new double[n];
 
-            double sum = 0;
-            for (int i = 0; i < vector.length; i ++) {
-                System.out.print("Enter a number: ");
-                vector[i] = sc.nextDouble();
-                sum += vector[i];
-            }
+                double sum = 0;
+                for (int i = 0; i < vector.length; i ++) {
+                    System.out.print("Enter a number: ");
+                    vector[i] = sc.nextDouble();
+                    sum += vector[i];
+                }
 
-            double avg = sum / vector.length;
+                double avg = sum / vector.length;
 
-            System.out.println();
-            System.out.printf("Values = %s", Arrays.toString(vector));
-            System.out.printf("\nSoma = %.2f", sum);
-            System.out.printf("\nMedia = %.2f", avg);
-            System.out.println();
+                System.out.println();
+                System.out.printf("Values = %s", Arrays.toString(vector));
+                System.out.printf("\nSoma = %.2f", sum);
+                System.out.printf("\nMedia = %.2f", avg);
+                System.out.println();
+
+                System.out.print("\nDo you want to repeat? (y/n) ");
+                response = sc.next();
+                System.out.println();
+
+                while (!response.equals("y") && !response.equals("n")) {
+                    System.out.println();
+                    System.out.print("Invalid! Please, enter only 'y' or 'no'.\n"
+                            + "Do you want to repeat? ");
+                    response = sc.next();
+                }
+
+            } while (response.equals("y"));
+
+            System.out.print(">>>>> Program terminated <<<<<");
 
             sc.close();
         }
