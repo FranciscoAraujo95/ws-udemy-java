196521c368d9271f50976955758e0c3c2f9e6e2a
Francisco <fhydglan@gmail.com>
2024-07-12 23:18:50 -0300

    exercise 007

---
+++ section-10-memorybehavior-arrays-lists/exercises/ex_007/src/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many elements will the vector have? ");
+        int n = sc.nextInt();
+        int[] elements = new int[n];
+
+        int sumElements = 0;
+        double quantityEven = 0;
+        for (int i = 0; i < elements.length; i++) {
+            System.out.print("Digit a number: ");
+            elements[i] = sc.nextInt();
+        }
+
+        for (int element : elements) {
+            if (element % 2 == 0) {
+                sumElements += element;
+                quantityEven++;
+            }
+        }
+
+        if (quantityEven == 0) {
+            System.out.print("No even numbers");
+        } else {
+            double averageEven = sumElements / quantityEven;
+
+            System.out.printf("Average of even numbers = %.1f", averageEven);
+        }
+
+        sc.close();
+    }
+}
---
+++ section-10-memorybehavior-arrays-lists/exercises/ex_007/src/entities/Vector.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Vector {
+    private int n;
+
+    public Vector(int n) {
+        this.n = n;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+}
