f84cdf43c222bf1c368ece33f9e9aa5666c68d99
Francisco <fhydglan@gmail.com>
2024-07-15 17:37:24 -0300

    solved the exercise again

--- section-10-memorybehavior-arrays-lists/exercises/List_exercise_my_version/src/application/Program.java
+++ section-10-memorybehavior-arrays-lists/exercises/List_exercise_my_version/src/application/Program.java
@@ -1,5 +1,9 @@
 package application;
 
+import entities.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,9 +13,47 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        List<Employee> list = new ArrayList<>();
 
+        System.out.print("How many employees will be registered? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.printf("Employee #%d:\n", i + 1);
+            System.out.print("Id: ");
+            int id = sc.nextInt();
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Salary: ");
+            double salary = sc.nextDouble();
+
+            Employee emp = new Employee(id, name, salary);
+
+            list.add(emp);
+            System.out.println();
+
+        }
+
+        System.out.print("Enter the employee id that will have the salary increase: ");
+        int idSalary = sc.nextInt();
+
+        Employee empId = list.stream().filter(x -> x.getId() == idSalary).findFirst().orElse(null);
+
+        if (empId == null) {
+            System.out.print("\nThis id does not exists.");
+        } else {
+            System.out.print("Enter the percentage: ");
+            double percent = sc.nextDouble();
+            empId.increaseSalary(percent);
+        }
+
+        System.out.println("\nList of employees: ");
+        for (Employee e : list) {
+            System.out.println(e);
+        }
 
         sc.close();
-
     }
 }
