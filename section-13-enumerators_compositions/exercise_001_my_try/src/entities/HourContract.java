719842f9fe861b77104e34552c5fc389dab7bf19
Francisco <fhydglan@gmail.com>
2024-07-17 12:52:27 -0300

    exercise 001 solved by me

---
+++ section-13-enumerators_compositions/exercise_001_my_try/src/entities/HourContract.java
@@ -0,0 +1,52 @@
+package entities;
+
+import java.util.Date;
+
+public class HourContract {
+
+    //Attributes
+    private Date date;
+    private Double valuePerHour;
+    private Integer hours;
+
+    //Constructors
+    public HourContract() {
+
+    }
+
+    public HourContract(Date date, Double valuePerHour, Integer hours) {
+        this.date = date;
+        this.valuePerHour = valuePerHour;
+        this.hours = hours;
+    }
+
+    //Methods
+    public Double totalValue() {
+        return valuePerHour * hours;
+    }
+
+    //Getters & Setters
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getValuePerHour() {
+        return valuePerHour;
+    }
+
+    public void setValuePerHour(Double valuePerHour) {
+        this.valuePerHour = valuePerHour;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+}
